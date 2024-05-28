4f0cb8a74142bd40f6b78f4f72eed5cea81ae6f4
Farhana <farhana.swe@gmail.com>
2024-05-27 19:34:41 -0700

    Update MovieListView.swift

--- StudioGhibli-SwiftUI/View/MovieListView.swift
+++ StudioGhibli-SwiftUI/View/MovieListView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct MovieListView: View {
+    @StateObject var viewModel: MoviesViewModel = MoviesViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Studio Ghibli films")
+            .font(.largeTitle)
+        
+        ScrollView {
+            ForEach(viewModel.movies) { movie in
+                AsyncImage(url: URL(string: movie.image)) { image in
+                    image.resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 12.0))
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+
+            
+        }
+        .task {
+            viewModel.getMovies()
+        }
     }
 }
 
