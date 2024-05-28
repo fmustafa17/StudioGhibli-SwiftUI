983e195979f2a7d0a46276c8f807e407e37fa410
Farhana <farhana.swe@gmail.com>
2024-05-27 19:41:34 -0700

    MovieDetailView

---
+++ StudioGhibli-SwiftUI/View/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  StudioGhibli-SwiftUI
+//
+//  Created by Farhana Mustafa on 5/27/24.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
