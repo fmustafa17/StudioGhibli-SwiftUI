58723dfa227ce07ab92152a43c49db845eb0e95e
Farhana <farhana.swe@gmail.com>
2024-05-27 19:37:04 -0700

    add lazyVGrid to make a 3 column view of images

--- StudioGhibli-SwiftUI/View/MovieListView.swift
+++ StudioGhibli-SwiftUI/View/MovieListView.swift
@@ -9,23 +9,26 @@ import SwiftUI
 
 struct MovieListView: View {
     @StateObject var viewModel: MoviesViewModel = MoviesViewModel()
+    
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+
     var body: some View {
         Text("Studio Ghibli films")
             .font(.largeTitle)
         
         ScrollView {
-            ForEach(viewModel.movies) { movie in
-                AsyncImage(url: URL(string: movie.image)) { image in
-                    image.resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 150)
-                        .clipShape(RoundedRectangle(cornerRadius: 12.0))
-                } placeholder: {
-                    ProgressView()
+            LazyVGrid(columns: columns) {
+                ForEach(viewModel.movies) { movie in
+                    AsyncImage(url: URL(string: movie.image)) { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 12.0))
+                    } placeholder: {
+                        ProgressView()
+                    }
                 }
             }
-
-            
         }
         .task {
             viewModel.getMovies()
