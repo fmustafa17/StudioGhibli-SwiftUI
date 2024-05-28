eb4eae1a6ba6998d16e2d2c8bc6364ad25f2b3c0
Farhana <farhana.swe@gmail.com>
2024-05-27 19:09:49 -0700

    Create MoviesViewModel.swift

---
+++ StudioGhibli-SwiftUI/ViewModel/MoviesViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesViewModel.swift
+//  StudioGhibli-SwiftUI
+//
+//  Created by Farhana Mustafa on 5/27/24.
+//
+
+import Combine
+import Foundation
+
+class MoviesViewModel: ObservableObject {
+    
+    @Published
+    
+    
+}
