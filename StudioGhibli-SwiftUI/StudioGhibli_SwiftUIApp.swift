b73e7c7882cf1da667b726654079e0b3b6d618eb
Farhana <farhana.swe@gmail.com>
2024-05-27 18:45:33 -0700

    first commit

---
+++ StudioGhibli-SwiftUI/StudioGhibli_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioGhibli_SwiftUIApp.swift
+//  StudioGhibli-SwiftUI
+//
+//  Created by Farhana Mustafa on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct StudioGhibli_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
