1a695290182de56f7b3ade1d138c9cc004d0e83b
Farhana <farhana.swe@gmail.com>
2024-05-27 19:06:06 -0700

    create MovieListView

--- StudioGhibli-SwiftUI/StudioGhibli_SwiftUIApp.swift
+++ StudioGhibli-SwiftUI/StudioGhibli_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StudioGhibli_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MovieListView()
         }
     }
 }
---
+++ StudioGhibli-SwiftUI/View/MovieListView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListView.swift
+//  StudioGhibli-SwiftUI
+//
+//  Created by Farhana Mustafa on 5/27/24.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListView()
+}
